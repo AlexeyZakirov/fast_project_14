8f53ed3bcf798ce2548f90b8db518b02abcbde1d
AlexeyZakirov <tinwhipcf@gmail.com>
2024-10-29 11:32:27 +0900

    init commit

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,17 +1,29 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
     @BeforeAll
     public static void setUp(){
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browserVersion = System.getProperty("version");
+        Configuration.browserSize = System.getProperty("browserSize");
         Configuration.baseUrl = "https://www.bork.ru/";
-        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
         Configuration.remote = String.format("https://%s@%s/wd/hub",
                 System.getProperty("auth"), System.getProperty("remoteUrl"));
     }
@@ -23,6 +35,17 @@ public class TestBase {
 
     @AfterEach
     public void tearDown(){
+        if (Configuration.browser.equals("firefox")) {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.addVideo();
+        } else {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+        }
+
         Selenide.closeWebDriver();
     }
 }
